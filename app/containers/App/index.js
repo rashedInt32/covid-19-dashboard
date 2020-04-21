c107039f80dc9522d38f5226ceb96f9615c59bb4
rashedInt32 <parvez08eee@gmail.com>
2020-04-21 12:21:02 +0600

    Remove firebase

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -14,16 +14,10 @@ import { ThemeProvider } from 'styled-components';
 import Router from 'containers/Router';
 import CountryContextProvider from 'containers/Context/CountryContext';
 
-import { initializeFirebase } from 'firebase/initializeFirebase';
-
 import GlobalStyle from '../../theme/globalStyles';
 import Theme from '../../theme';
 
 export default function App() {
-  useEffect(() => {
-    initializeFirebase();
-  }, []);
-
   return (
     <>
       <CountryContextProvider>
