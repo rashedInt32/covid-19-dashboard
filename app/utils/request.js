bd93d7c9db693fcde2b603529bae71bdc3a6e5af
rashedInt32 <parvez08eee@gmail.com>
2020-04-22 10:29:23 +0600

    Added access control origin

--- app/utils/request.js
+++ app/utils/request.js
@@ -4,6 +4,10 @@ import { baseURL } from 'utils/apiRoutes';
 const request = axios.create({
   baseURL,
   timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+  },
 });
 
 export { request };
